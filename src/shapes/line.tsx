aa3630d58ad991e8c672b2d6356c2c075646a4f9
Zachary Masaitis <legomaster119@gmail.com>
2026-08-16 12:15:02 -0400

    Create 2-dimensional line component

--- src/shapes/line.tsx
+++ src/shapes/line.tsx
@@ -28,3 +28,51 @@ export function XLine(props: {x0: number, x1: number, y?: number, weight?: numbe
     </>
   )
 }
+
+function linspace(startValue: number, stopValue: number, cardinality: number) {
+  var arr = []
+  var step = (stopValue - startValue) / (cardinality - 1)
+  for (var i = 0; i < cardinality; i++) {
+    arr.push(startValue + (step * i))
+  }
+  return arr;
+}
+
+export function Line(props: {
+  x0: number,
+  x1: number,
+  y0: number,
+  y1: number,
+  weight?: number
+}) {
+  const weight = props.weight ?? 5
+
+  let [x0, x1] = [props.x0, props.x1]
+  if (x0 > x1) { [x0, x1] = [x1, x0] }
+
+  let [y0, y1] = [props.y0, props.y1]
+  if (y0 > y1) { [y0, y1] = [y1, y0] }
+
+  const numStepsX = x1 - x0
+  const numStepsY = y1 - y0
+  const numSteps = Math.max(numStepsX, numStepsY)
+
+  const xValues = linspace(x0, x1, numSteps)
+  const yValues = linspace(y0, y1, numSteps)
+
+  return (
+    <>
+      {
+        [...Array(numSteps).keys()].map(ix => {
+          return (
+            <Dot
+              diameter={weight}
+              x={xValues[ix]}
+              y={yValues[ix]}
+            />
+          )
+        })
+      }
+    </>
+  )
+}

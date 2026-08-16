8eec9b7d7b8de66a3e59f9c88aac8263f336d6d7
Zachary Masaitis <legomaster119@gmail.com>
2026-08-16 11:10:26 -0400

    Create Archimedean spiral shape using Dots

---
+++ src/shapes/spiral.tsx
@@ -0,0 +1,28 @@
+import { Dot } from "./dot"
+
+export function Spiral(props: {weight?: number, size?: number, startDegree?: number, endDegree?: number}) {
+  const weight = props.weight ?? 5
+  const size = props.size ?? 12
+  const startDegree = props.startDegree ?? 0
+  const endDegree = props.endDegree ?? 360 * 8
+
+  return (
+    <>
+      {
+        [...Array(endDegree).keys()].map(deg => {
+          if (deg <= startDegree) { return }
+          
+          const theta = (deg / 2) * Math.PI / 180
+
+          return (
+            <Dot
+              diameter={weight}
+              y={size * theta * Math.sin(theta)}
+              x={size * theta * Math.cos(theta)}
+            />
+          )
+        })
+      }
+    </>
+  )
+}

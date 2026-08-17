100d912753a5b374ab7606ae86db0ba2824c674c
Zachary Masaitis <legomaster119@gmail.com>
2026-08-17 11:55:53 -0400

    Allow custom dot colors

--- src/shapes/dot.tsx
+++ src/shapes/dot.tsx
@@ -1,19 +1,19 @@
-export function Dot(props: {diameter: number, x: number, y: number}) {
+export function Dot(props: {diameter: number, x: number, y: number, color?: string}) {
   const radius = props.diameter / 2
   const center = {
     x: window.outerWidth / 2,
     y: window.outerHeight / 2
   }
   const offset = {
-    x: 0,
-    y: -50
+    x: -18,
+    y: -43
   }
 
   return (
     <div
       className={`rounded-full`}
       style={{
-        "backgroundColor": "#000",
+        "backgroundColor": props.color ?? "#000",
         "borderRadius": 9999999,
         "position": "absolute",
         "height": props.diameter,

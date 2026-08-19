5e663c02322be6e69d0c3e4bbb9f01e9e093e103
Zachary Masaitis <legomaster119@gmail.com>
2026-08-19 11:57:57 -0400

    Brainstorm puzzle ideas

--- src/components/puzzle_board.tsx
+++ src/components/puzzle_board.tsx
@@ -2,10 +2,36 @@ import { puzzleParams } from "../math"
 import { Spiral } from "../shapes/spiral"
 import { SpiralSpokes } from "./spiral_spokes"
 import { LetterDisplay } from "./letter_display"
-import { useState } from "react"
 
-export function PuzzleBoard() {
-  const [word, setWord] = useState<string>("abcdefghijklmnopqrstuvwxyz")
+export function PuzzleBoard(props: {
+  word: string,
+  setWord: React.Dispatch<React.SetStateAction<string>>,
+  selectedLetter: number,
+  setSelectedLetter: React.Dispatch<React.SetStateAction<number>>
+}) {
+  // all that glitters is not gold
+  // absolute temperature scale
+  // artery of the vestibule bulb
+  // battle of the ardennes bulge
+  // brassica bleracea botrytis
+  // capital of st kitts and nevis
+  // capital of marshall islands
+  // coefficient of correlation
+  // coefficient of performance
+  // coordinated universal time
+  // driving under the influence
+  // economic and social council
+  // electronic reconnaissance
+  // thermodynamic equilibrium
+  // mathematical statistician
+  //  -> emancipation proclamation
+  //    -> first law of thermodynamics
+  //  -> hydrogen ion concentration
+  //  -> internal combustion engine
+  //  -> lactobacillus acidophilus
+
+  const word = props.word
+  const setWord = props.setWord
 
   return (
     <>
@@ -17,7 +43,7 @@ export function PuzzleBoard() {
 
       <SpiralSpokes/>
 
-      <LetterDisplay longWord={word} setWord={setWord}/> 
+      <LetterDisplay longWord={word} setWord={setWord} selectedLetter={props.selectedLetter} setSelectedLetter={props.setSelectedLetter}/>
     </>
   )
 }

4dbbe2fb62c75422f22d1f60c2d84e0b30735c81
Zachary Masaitis <legomaster119@gmail.com>
2026-08-22 07:34:03 -0400

    Implement Number shape

---
+++ src/shapes/number.tsx
@@ -0,0 +1,16 @@
+import { center, offset } from "../math"
+import type { NumberProps } from "./types"
+
+export function Number(props: NumberProps) {
+  return (
+    <div
+      style={{
+        "position": "absolute",
+        "left": center.x + props.x + offset.x - 5,
+        "top": center.y - props.y + offset.y - 10
+      }}
+    >
+      {props.num > 0 && props.num}
+    </div>
+  )
+}

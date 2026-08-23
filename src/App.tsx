0a8f34e9978852c152909ead0706ab8128259c02
Zachary Masaitis <legomaster119@gmail.com>
2026-08-23 13:51:46 -0400

    Use puzzle.json file for puzzle data

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,25 @@ import { useState } from "react"
 import { PuzzleBoard } from "./components/puzzle_board"
 import { SpiralWord } from "./components/spiral_word"
 import { range, puzzleParams } from "./math"
+import { ClueDisplay } from "./components/clue_display"
 
 
 function App() {
-  const [word, setWord] = useState<string>("FIRSTLAWOFTHERMODYNAMICS")
+  const [word, setWord] = useState<string>("")
   const [selectedLetter, setSelectedLetter] = useState<number>(-1)
   const degreeStep = 360 / puzzleParams.numSpokes
   const maxLetters = range(puzzleParams.spiralStartDegree, puzzleParams.spiralEndDegree, degreeStep).length
 
   return (
     <>
+      <div style={{
+        "float": "right"
+      }}>
+        <ClueDisplay selectedLetter={selectedLetter} setSelectedLetter={setSelectedLetter}/>
+      </div>
       <PuzzleBoard word={word} setWord={setWord} selectedLetter={selectedLetter} setSelectedLetter={setSelectedLetter}/>
       <SpiralWord word={word} length={maxLetters} selectedLetter={selectedLetter} setSelectedLetter={setSelectedLetter}/>
+
     </>
   )
 }

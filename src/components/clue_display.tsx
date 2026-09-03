8a58501d1368fce2d4d155e5f8b94cf780ee6062
Zachary Masaitis <legomaster119@gmail.com>
2026-09-03 14:36:30 -0400

    Add capability for puzzle themed clue

--- src/components/clue_display.tsx
+++ src/components/clue_display.tsx
@@ -3,30 +3,52 @@ import type { PuzzleContext } from "../types"
 
 export function ClueDisplay(props: {puzzleContext: PuzzleContext}) {
   return (
-    <div className="flex flex-col gap-y-1 justify-center m-10">
-      {
-        puzzle.clues.map(clue => {
-          return (
-            <div 
-              className={
-                props.puzzleContext.selectedLetter == clue.char ||
-                props.puzzleContext.selectedLetter == clue.char - 8 ||
-                props.puzzleContext.selectedLetter == clue.char - 16
-                 ? "bg-yellow-300" : ""
-              }
-              onClick={() => {
-                if (props.puzzleContext.selectedLetter != clue.char) {
-                  props.puzzleContext.setSelectedLetter(clue.char)
-                } else {
-                  props.puzzleContext.setSelectedLetter(-1)
-                }
-              }}
-            >
-              {clue.number}. {clue.text}
-            </div>
-          )
-        })
-      }
+    <div className="flex flex-col justify-center">
+      <div className="flex flex-col justify-center m-10">
+        <div className="font-bold italic">
+          Inwards & Outwards
+        </div>
+        {
+          puzzle.clues.map(clue => {
+            if (clue.number === 0) { return }
+            
+            const highlighted = props.puzzleContext.selectedLetter == clue.char ||
+            props.puzzleContext.selectedLetter == clue.char - 8 ||
+            props.puzzleContext.selectedLetter == clue.char - 16
+            
+            return (
+              <div 
+                className={`p-1 ${highlighted ? "bg-yellow-300" : ""}`}
+                onClick={() => {
+                  if (props.puzzleContext.selectedLetter != clue.char) {
+                    props.puzzleContext.setSelectedLetter(clue.char)
+                  } else {
+                    props.puzzleContext.setSelectedLetter(-1)
+                  }
+                }}
+              >
+                {clue.number}. {clue.text}
+              </div>
+            )
+          })
+        }
+      </div>
+      <div className="flex flex-col justify-center mx-10">
+        {
+          puzzle.clues.map(clue => {
+            if (clue.number !== 0) { return }
+
+            return (
+              <div>
+                <div className="font-bold italic">
+                  Puzzle Clue
+                </div>
+                {clue.text}
+              </div>
+            )
+          })
+        }
+      </div>
     </div>
   )
 }

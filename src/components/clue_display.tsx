05f688dc85da40dfb14b3447ce87c5a4d53810d7
Zachary Masaitis <legomaster119@gmail.com>
2026-09-09 13:23:05 -0400

    Show colors specific to each clue set; implement clue highlighting

--- src/components/clue_display.tsx
+++ src/components/clue_display.tsx
@@ -1,31 +1,59 @@
 import puzzle from "../puzzle.json"
 import type { PuzzleContext } from "../types"
+import { letterColorSets } from "./letter_display"
+
+const numCellsPerRotation = puzzle.dimensions.num_spokes
+
+export function charBelongsToClue(selectedChar: number, clueChar: number): boolean {
+  if (selectedChar === clueChar) {
+    return true
+  }
+  else if (selectedChar < clueChar) {
+    for (let test = clueChar; test >= 0; test -= numCellsPerRotation) {
+      if (selectedChar === test) { return true }
+    }
+  }
+  else if (selectedChar > clueChar) {
+    for (let test = clueChar; test <= puzzle.answer.length; test += numCellsPerRotation) {
+      if (selectedChar === test) { return true}
+    }
+  }
+  return false
+}
+
+export const clueSets = Array.from([...new Set(puzzle.clues.map(clue => clue.direction))])
+
 
 export function ClueDisplay(props: {puzzleContext: PuzzleContext}) {
   let directions = puzzle.clues.map(clue => clue.direction)
-  directions = Array.from([...new Set(directions)]).toSorted()
+  directions = Array.from([...new Set(directions)])
 
   return (
     <div className="flex flex-col justify-center">
       <div className="flex flex-col justify-center gap-y-2 m-10">
         {
-          directions.map(direction => {
+          directions.map((direction, ix) => {
+            const directionColor = letterColorSets[ix]
+
             return (
               <div>
-                <div className="font-bold italic">
+                <div
+                  style={{backgroundColor: directionColor}}
+                  className="px-1 font-bold italic"
+                >
                   {direction.toUpperCase()}
                 </div>
                 {
                   puzzle.clues.filter(clue => clue.direction === direction).map(clue => {
-                    if (clue.number === 0) { return (<>{clue.text}</>) }
+                    if (clue.number === 0) { return (
+                      <div className="px-6">{clue.text}</div>
+                    ) }
             
-                    const highlighted = props.puzzleContext.selectedLetter == clue.char ||
-                    props.puzzleContext.selectedLetter == clue.char - 8 ||
-                    props.puzzleContext.selectedLetter == clue.char - 16
+                    const highlighted = charBelongsToClue(props.puzzleContext.selectedLetter, clue.char)
                     
                     return (
                       <div 
-                        className={`p-1 ${highlighted ? "bg-yellow-300" : ""}`}
+                        className={`p-1 cursor-pointer ${highlighted ? "bg-yellow-300" : ""}`}
                         onClick={() => {
                           if (props.puzzleContext.selectedLetter != clue.char) {
                             props.puzzleContext.setSelectedLetter(clue.char)

cb9f8941f070ac60ae7650811aa5b2ea6360f26f
Zachary Masaitis <legomaster119@gmail.com>
2026-09-12 09:44:23 -0400

    Add type for Clue; add props for edit direction & current clue in puzzle context object

--- src/types.ts
+++ src/types.ts
@@ -10,4 +10,15 @@ export interface PuzzleContext {
   setWord: React.Dispatch<React.SetStateAction<string>>,
   selectedLetter: number,
   setSelectedLetter: React.Dispatch<React.SetStateAction<number>>
+  editDirection: string,
+  setEditDirection: React.Dispatch<React.SetStateAction<string>>
+  currentClue: Clue | undefined,
+  setCurrentClue: React.Dispatch<React.SetStateAction<Clue | undefined>>
+}
+
+export interface Clue {
+  char: number,
+  direction: string,
+  number: number,
+  text: string
 }

b93ec85f640f98309c6c2b2fac886b96082b45c1
Zachary Masaitis <legomaster119@gmail.com>
2026-09-12 09:46:48 -0400

    Add new context items to global PuzzleContext object

--- src/App.tsx
+++ src/App.tsx
@@ -2,35 +2,40 @@ import { useState } from "react"
 import { PuzzleBoard } from "./components/puzzle_board"
 import { SpiralWord } from "./components/spiral_word"
 import { ClueDisplay } from "./components/clue_display"
-import type { PuzzleContext } from "./types"
+import type { Clue, PuzzleContext } from "./types"
 import puzzle from "./puzzle.json"
 import { Dialog } from "./components/dialog"
+import { WriteOptions } from "./components/write_options"
 
 
 function App() {
   const [word, setWord] = useState<string>(" ".repeat(puzzle.answer.length))
   const [selectedLetter, setSelectedLetter] = useState<number>(-1)
   const [showFinishedDialog, setShowFinishedDialog] = useState<boolean>(true)
+  const [editDirection, setEditDirection] = useState<string>("always-outwards")
+  const [currentClue, setCurrentClue] = useState<Clue | undefined>()
 
   const puzzleContext: PuzzleContext = {
     word, setWord,
-    selectedLetter, setSelectedLetter
+    selectedLetter, setSelectedLetter,
+    editDirection, setEditDirection,
+    currentClue, setCurrentClue
   }
 
   const isCorrect = word.toUpperCase() == puzzle.answer.toUpperCase()
-
+  
   return (
     <>
       <div className="border-black border-b-2 h-[4rem] text-2xl font-bold flex items-center p-4">wordcoil</div>
-      <div
-        className="m-10`" 
-        style={{"float": "right"}}
-      >
+      
+      <div style={{float: "right"}}>
         <ClueDisplay puzzleContext={puzzleContext}/>
       </div>
+      
       <div style={{position: "relative"}}>
         <PuzzleBoard puzzleContext={puzzleContext}/>
       </div>
+      
       <SpiralWord puzzleContext={puzzleContext}/>
 
       <Dialog show={isCorrect && showFinishedDialog}>
@@ -41,6 +46,10 @@ function App() {
           Congratulations! You completed the puzzle!
         </div>
       </Dialog>
+
+      <div style={{float: "left"}}>
+        <WriteOptions puzzleContext={puzzleContext}/>
+      </div>
 
     </>
   )
